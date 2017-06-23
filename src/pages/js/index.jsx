a8373a2695b366264309ded296a9390bcd43d502
Sergey Makoveev <sergey@makoveev.info>
2017-06-23 10:45:34 +0300

    eslint: добавил no-use-before-define

--- src/pages/js/index.jsx
+++ src/pages/js/index.jsx
@@ -8,6 +8,57 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+)
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+    </div>
+)
+
+
+const Topic = ({ match = {} } = {}) => (
+    <div>
+        <h3>{R.prop('topicId', match.params)}</h3>
+    </div>
+)
+
+
+const Topics = ({ match = {} } = {}) => (
+    <div>
+        <h2>Topics</h2>
+        <ul>
+            <li>
+                <Link to={`${match.url}/rendering`}>
+                    Rendering with React
+                </Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/components`}>
+                    Components
+                </Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/props-v-state`}>
+                    Props v. State
+                </Link>
+            </li>
+        </ul>
+        <Route
+            path={`${match.url}/:topicId`}
+            component={Topic}/>
+        <Route
+            exact
+            path={match.url}
+            render={() => <h3>Please select a topic.</h3>}/>
+    </div>
+)
+
+
 class Layout extends React.Component {
 
     state = {
@@ -57,52 +108,5 @@ class Layout extends React.Component {
     )
 }
 
-const Home = () => (
-    <div>
-        <h2>Home</h2>
-    </div>
-)
-
-const About = () => (
-    <div>
-        <h2>About</h2>
-    </div>
-)
-
-const Topics = ({ match = {} } = {}) => (
-    <div>
-        <h2>Topics</h2>
-        <ul>
-            <li>
-                <Link to={`${match.url}/rendering`}>
-                    Rendering with React
-                </Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/components`}>
-                    Components
-                </Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/props-v-state`}>
-                    Props v. State
-                </Link>
-            </li>
-        </ul>
-        <Route
-            path={`${match.url}/:topicId`}
-            component={Topic}/>
-        <Route
-            exact
-            path={match.url}
-            render={() => <h3>Please select a topic.</h3>}/>
-    </div>
-)
-
-const Topic = ({ match = {} } = {}) => (
-    <div>
-        <h3>{R.prop('topicId', match.params)}</h3>
-    </div>
-)
 
 ReactDOM.render(<Layout />, document.getElementById('layout'));
