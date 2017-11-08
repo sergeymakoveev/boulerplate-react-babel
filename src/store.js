2fecb7cfb8c1638a2852607ad6c40270b8c2b2d0
Sergey Makoveev <sergey@makoveev.info>
2017-11-08 13:31:45 +0300

    store: rename thunkMiddleware -> thunk

--- src/store.js
+++ src/store.js
@@ -2,19 +2,19 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducer as form } from 'redux-form';
 import { createLogger } from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
+import thunk from 'redux-thunk';
 
 import { reducer as users } from 'pages/users/reducer';
 
 
-const store = createStore(
+export const store = createStore(
     combineReducers({
         form,
         users,
     }),
     composeWithDevTools(
         applyMiddleware (
-            thunkMiddleware,
+            thunk,
             createLogger(),
         )
     )
