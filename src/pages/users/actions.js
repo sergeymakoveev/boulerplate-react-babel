e080a916b7b404d1d9f48c4f356a2756f889448f
Sergey Makoveev <sergey@makoveev.info>
2017-11-13 10:29:09 +0300

    redux: add USERS_CREATE,     USERS_LIST, USERS_ITEM, USERS_UPDATE

--- src/pages/users/actions.js
+++ src/pages/users/actions.js
@@ -2,20 +2,54 @@ import api from 'api/http';
 
 
 export const TYPES = {
-    USERS_SHOW: 'USERS_SHOW',
+    USERS_CREATE: 'USERS_CREATE',
+    USERS_LIST: 'USERS_LIST',
+    USERS_ITEM: 'USERS_ITEM',
+    USERS_UPDATE: 'USERS_UPDATE',
 };
 
 
 export const ACTIONS = {
-    USERS_SHOW:
+
+    USERS_LIST:
+        () =>
+        (dispath) => (
+            api.users
+                .list()
+                .then((data) => dispath({ type: TYPES.USERS_LIST, data }))
+        ),
+
+    USERS_CREATE:
+        (src) =>
+        (dispath) => (
+            api.users
+                .create(src)
+                .then((data) => dispath({ type: TYPES.USERS_CREATE, data }))
+        ),
+
+    USERS_UPDATE:
+        (id, src) =>
+        (dispath) => (
+            api.users
+                .update(id)(src)
+                .then(
+                    (data) => {
+                        dispath({ type: TYPES.USERS_UPDATE, data });
+                        dispath(ACTIONS.USERS_LIST);
+                    }
+                )
+        ),
+
+    USERS_ITEM:
         (id) =>
-            (dispath) => (
-                +id
-                ? api.users
-                    .item(id)
-                    .then((data) => dispath({ type: TYPES.USERS_SHOW, data }))
-                : dispath({ type: TYPES.USERS_SHOW, data: {} })
-            )
+        (dispath) => (
+            +id
+            ? api.users
+                .item(id)
+                .then((data) => dispath({ type: TYPES.USERS_ITEM, data }))
+            : dispath({ type: TYPES.USERS_ITEM, data: {} })
+        ),
+
 };
 
 
--- src/pages/users/reducer.js
+++ src/pages/users/reducer.js
@@ -3,8 +3,12 @@ import { TYPES } from './actions';
 
 export const reducer = (state = {}, action) => {
     switch (action.type) {
-        case TYPES.USERS_SHOW:
-            return { ...state, show: action.data };
+        case TYPES.USERS_CREATE:
+        case TYPES.USERS_ITEM:
+        case TYPES.USERS_UPDATE:
+            return { ...state, item: action.data };
+        case TYPES.USERS_LIST:
+            return { ...state, list: action.data };
         default:
             return state;
     }
