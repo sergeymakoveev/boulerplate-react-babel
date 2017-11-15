50691271d4168efe6044a2727f65ea6d8faffd4b
Sergey Makoveev <sergey@makoveev.info>
2017-11-15 16:35:25 +0300

    user/reducer: small changes

--- src/pages/users/reducer.js
+++ src/pages/users/reducer.js
@@ -2,13 +2,14 @@ import { TYPES } from './actions';
 
 
 export const reducer = (state = {}, action) => {
+    const { data } = action;
     switch (action.type) {
         case TYPES.USERS_CREATE:
         case TYPES.USERS_ITEM:
         case TYPES.USERS_UPDATE:
-            return { ...state, item: action.data };
+            return { ...state, item: data };
         case TYPES.USERS_LIST:
-            return { ...state, list: action.data };
+            return { ...state, list: data };
         case TYPES.USERS_REMOVE:
         default:
             return state;
