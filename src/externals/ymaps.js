29d8398ef40be327a2477ca3a5c8187b6e26bdcd
Sergey Makoveev <sergey@makoveev.info>
2018-06-05 23:52:20 +0300

    add Yandex Maps API loader

---
+++ src/externals/ymaps.js
@@ -0,0 +1,16 @@
+import promisescript from 'promisescript';
+
+
+const YMLoader =
+        promisescript({
+                url: '//api-maps.yandex.ru/2.1/?lang=ru_RU',
+                type: 'script',
+                exposed: 'ymaps'
+            })
+            .then(() => new Promise(
+                            resolve => window.ymaps
+                                             .ready( () => resolve({ api: window.ymaps }) )
+                         ))
+            .catch( () => { throw new Error('Error by load Yandex Maps API'); });
+
+export default YMLoader;
