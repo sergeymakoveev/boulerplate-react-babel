b15f36bb17c8c6131f78c1a1afaff38a1598d56e
Sergey Makoveev <sergey@makoveev.info>
2018-06-06 00:05:37 +0300

    add file-saver

---
+++ src/externals/file-saver.js
@@ -0,0 +1,15 @@
+import * as R from 'ramda';
+import { saveAs } from 'file-saver';
+
+export const save_response =
+    R.curry(
+        ({ filename, response }) => (
+            response
+                .blob()
+                .then(blob => saveAs(blob, filename))
+        )
+    );
+
+export default {
+    response: save_response
+}
