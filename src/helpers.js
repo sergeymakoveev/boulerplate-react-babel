1f37d5f10fcce42501960086d8ea6906d5756599
Sergey Makoveev <sergey@makoveev.info>
2018-11-09 09:10:18 +0300

    #2 add stopPropagation helper

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,20 @@
 import * as R from 'ramda';
+import fp from 'lodash/fp';
 
 
 export const path =
     (data) =>
         (p) => R.flip(R.path)(data, p.split('.'));
 
+export const stopPropagation =
+    (handler) =>
+        (e) => {
+            fp.isFunction(e.stopPropagation)
+                && e.stopPropagation();
+            return handler(e);
+        };
+
+
 export default {
-    path,
+    path, stopPropagation,
 };
