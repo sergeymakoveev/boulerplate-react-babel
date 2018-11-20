b60f801344db1c523645fcdd2aa35e65c4ecba58
Sergey Makoveev <sergey@makoveev.info>
2018-11-20 14:40:10 +0300

    #4 <Loading /> - fix styles

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 
+const size = 100;
+
 const Loading = () => (
-    <CircularProgress color="secondary" />
+    <CircularProgress
+        style={{
+            position: 'fixed',
+            width: `${size}px`,
+            height: `${size}px`,
+            top: '50%',
+            left: '50%',
+            marginTop: `-${size / 2}px`,
+            marginLeft: `-${size / 2}px`,
+        }}
+    />
 );
 
 export default Loading;
