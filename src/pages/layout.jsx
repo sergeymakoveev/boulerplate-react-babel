f0a449909b5f8c31da326d7c4d7d9d740da5fb79
Sergey Makoveev <sergey@makoveev.info>
2018-11-23 14:03:05 +0300

    [#4] выделил <Layout />, применил к нему Loadable

---
+++ src/pages/layout.jsx
@@ -0,0 +1,122 @@
+import fp from 'lodash/fp';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Switch } from 'react-router-dom';
+
+import AppBar from '@material-ui/core/AppBar';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+import { withStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconMenu from '@material-ui/icons/Menu';
+
+import { routes, PageAbout, PageHome, PageUser, PageUsers, PageExperiments } from 'pages';
+
+import { MenuDrawer, MenuToolbar } from 'components';
+import { stopPropagation } from 'helpers/dom';
+
+
+const styles_layout = (theme) => ({
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+    },
+    main: {
+        padding: theme.spacing.unit * 3,
+    },
+    toolbar: theme.mixins.toolbar,
+    title: {
+        flexGrow: 1,
+    },
+    overlay: {
+        height: '100%',
+    },
+});
+
+class Layout extends React.Component {
+    static propTypes = {
+        classes: PropTypes.shape({
+            appBar: PropTypes.string,
+            main: PropTypes.string,
+            overlay: PropTypes.string,
+        }),
+    };
+
+    state = {
+        drawer: false,
+        title: 'React-Babel boulerplate',
+    };
+
+    toggleDrawer = (state) => () => {
+        this.setState(({ drawer }) => ({
+            drawer: fp.isUndefined(state) ? !drawer : state,
+        }));
+    };
+
+    render() {
+        const { title, drawer } = this.state;
+        const { classes } = this.props;
+        return (
+            <div
+                role="presentation"
+                className={classes.overlay}
+                onClick={this.toggleDrawer(false)}
+            >
+                <AppBar position="fixed" className={classes.appBar}>
+                    <Toolbar disableGutters>
+                        <IconButton
+                            color="inherit"
+                            aria-label="Menu"
+                            onClick={stopPropagation(this.toggleDrawer())}
+                        >
+                            <IconMenu />
+                        </IconButton>
+                        <Typography
+                            className={classes.title}
+                            variant="h6"
+                            color="inherit"
+                            noWrap
+                        >
+                            {title}
+                        </Typography>
+                        <MenuToolbar />
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    variant="persistent"
+                    anchor="left"
+                    open={drawer}
+                >
+                    <div className={classes.toolbar} />
+                    <MenuDrawer />
+                </Drawer>
+                <main className={classes.main}>
+                    <div className={classes.toolbar} />
+                    <Switch>
+                        <Route path={routes.home()} exact component={PageHome} />
+                        <Route path={routes.about()} component={PageAbout} />
+                        <Route path={routes.users()} component={PageUsers} />
+                        <Route path="/experiments" component={PageExperiments} />
+                        <Route
+                            path="*"
+                            render={
+                                ({ location }) => (
+                                    <h3>
+                                        404:
+                                        {location.pathname}
+                                    </h3>
+                                )
+                            }
+                        />
+                    </Switch>
+                    <Route path={routes.users(':id')} exact component={PageUser} />
+                </main>
+            </div>
+        );
+    }
+}
+
+const LayoutStyled = withStyles(styles_layout, { withTheme: true })(Layout);
+
+export default LayoutStyled;
